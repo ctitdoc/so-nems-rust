77e130c2c03b869318c65e07350c7d4eddb520ec
itdoc <anto.delahaye@gmail.com>
2022-03-22 18:09:02 +0100

    factorisation du code qui génère la FAQ, la partie Concept (qui regroupe le concept et la partie livraison) et le footer donc chacune des parties de la homapage se retrouvent dans une différente fonction dans une impl App

--- src/main.rs
+++ src/main.rs
@@ -14,16 +14,18 @@ pub struct Video {
     prenom: String,
     date_naissance: String,
     numero_tel: String,
-    adresse_mail:String,
+    adresse_mail: String,
     mot_de_passe: String,
     confirmation_mp: String,
-    adresse : String,
+    adresse: String,
 
 }
+
 #[derive(Clone, PartialEq, Deserialize)]
 pub struct Produit {
     nom_produit: String,
 }
+
 #[derive(Clone, PartialEq, Deserialize)]
 pub struct Commande {
     quantite_cmd: i32,
@@ -39,22 +41,22 @@ pub enum Msg {
     GetCommande,
     UpdateMemberList(Vec<Video>),
     UpdateCmdList(Vec<Commande>),
-    UpdateProdList(Vec<Produit>)
-
+    UpdateProdList(Vec<Produit>),
 }
 
 pub struct App {
-    value: i64, // This will store the counter value
-    videos : Vec<Video>,
+    value: i64,
+    // This will store the counter value
+    videos: Vec<Video>,
     products: Vec<Produit>,
     commande: Vec<Commande>,
 }
 
 async fn wrap<F: std::future::Future>(f: F, the_callback: yew::Callback<F::Output>) {
-        console::log!("execution START of wrap fn...");
-        the_callback.emit(f.await);
-        console::log!("execution END of wrap fn...");
-    }
+    console::log!("execution START of wrap fn...");
+    the_callback.emit(f.await);
+    console::log!("execution END of wrap fn...");
+}
 
 impl App {
     fn get_html_member_list(&self, ctx: &Context<Self>) -> Html {
@@ -73,7 +75,7 @@ impl App {
         ).collect::<Html>();
         /*{format!("{}: {} {} {} {} {} {} {}", video.nom, video.prenom, video.date_naissance, video.numero_tel,
                  video.adresse_mail, video.mot_de_passe, video.confirmation_mp, video.adresse)}*/
-html! {
+        html! {
         <table>
             <thead>
             <tr>
@@ -88,14 +90,14 @@ html! {
     }
 
     fn get_html_product_list(&self, ctx: &Context<Self>) -> Html {
-       let rows = self.products.iter().map(|produit| html! {
+        let rows = self.products.iter().map(|produit| html! {
            <tr>
            <td>{&produit.nom_produit}</td>
            </tr>
     //<p>{format!("{}", produit.nom_produit)}</p>
 }).collect::<Html>();
 
-    html! {
+        html! {
         <table>
             <thead>
             <tr>
@@ -107,9 +109,9 @@ html! {
     </tbody>
     </table>
     }
-}
-fn get_html_cmd_list (&self, ctx: &Context<Self>) -> Html {
-    let rows = self.commande.iter().map(|commande| html! {
+    }
+    fn get_html_cmd_list(&self, ctx: &Context<Self>) -> Html {
+        let rows = self.commande.iter().map(|commande| html! {
            <tr>
            <td>{&commande.quantite_cmd}</td>
             <td>{&commande.member_id}</td>
@@ -117,37 +119,163 @@ fn get_html_cmd_list (&self, ctx: &Context<Self>) -> Html {
     //commande.quantite_cmd, commande.member_id
 }).collect::<Html>();
 
-    html! {
+        html! {
         <table>
             <thead>
-            <tr>
-            <th> {"Affichage commande"}</th>
-            </tr>
+                <tr>
+                    <th> {"Affichage commande"}</th>
+                </tr>
             </thead>
             <tbody>
-    {rows}
-    </tbody>
+                {rows}
+            </tbody>
+        </table>
+        }
+    }
+    fn get_html_accueil(&self, ctx: &Context<Self>) -> Html {
+        html! {
+    <header>
+        <div class = "nav-img">
+            <div class = "img-pres" >
+        </div>
+        <div class = "title">
+            <h1>{"Sô Nems"}</h1>
+            <hr color = "black"/>
+            <h2>{"spécialité maison"}</h2>
+        </div>
+        </div>
+    </header>
+    }
+    }
+
+    fn get_html_nav(&self, ctx: &Context<Self>)-> Html{
+        html!{
+            <header>
+  <nav class="navbar-part">
+    <div class="container">
+      <div class="navbar-content">
+        <a href="#">
+          <img src="./img/nems-logo.jpg" alt=""/>
+        </a>
+        <h1> {"Sô Nems.fr"}</h1>
+
+
+        <div class="navbar-links">
+          <ul class="navbar-link">
+            <a href="index.html">
+              <li class="navbar-item"> {"Acceuil"}</li>
+            </a>
+            <a href="La-carte.html">
+              <li class="navbar-item">{"La Carte"}</li>
+            </a>
+            <a href="Annonce">
+              <li class="navbar-item">{"Annonce"}</li>
+            </a>
+            <a href="#contact">
+              <li class="navbar-item">{"Contact"}</li>
+            </a>
+            <a href="mon-compte.html">
+              <li class="fifth-link">{"Mon Compte"}</li>
+            </a>
+            <a href = "#FAQbis"> {"FAQ"} </a>
+          /*  <a href="test-yew.html">
+              <li class="fifth-link">{"test yew"}</li>
+            </a>
+            <a href="content_compte.html">
+              <li class="fifth-link">{"contenu d'un compte"}</li>
+            </a>*/
+            <a href = "#" onclick={ctx.link().callback(|_| Msg::GetMembers)}> {"liste des membres"}</a>
+             <a href = "#" onclick={ctx.link().callback(|_| Msg::GetCommande)}> {"Commande"}</a>
+             <a href = "#" onclick={ctx.link().callback(|_| Msg::GetProducts)}> {"liste produit"}</a>
+
+
+
+          </ul>
+
+        </div>
+      </div>
+    </div>
+  </nav>
+
+</header>
+        }
+    }
+    fn get_html_concept (&self, ctx: &Context<Self>)-> Html{
+        html!{
+        <div class="Colonne">
+      <div class="Concept">
+        <div class="desc-conc">
+          <h2>{"Sô Nems ?"}</h2>
+          <p>{"**************************************** "}</p>
+          <p>{"******************************************."}</p>
+          <p>{"***************************************************"}</p>
+        </div>
+            </div>
+
+      <div class="Livraison">
+        <div class="desc-livr">
+          <h2> {"Livraison"} </h2>
+          <p>{"Perimètre de livraison : Crolles"}</p>
+          <p>{"Numéro livreur : 01.02.03.04.05"}</p>
+        </div>
+      <div class="img-livr"><img src="./img/dark-scoot.png"/></div>
+
+
+      </div>
+    </div>
+        }
+    }
+    fn get_html_faq(&self, ctx: &Context<Self>) -> Html{
+        html! {
+    <section id = "FAQbis">
+    <div class="FAQ">
+    <div class="FAQ-content">
+    <h2> {"F.A.Q"}</h2>
+    <p>{"Les ingrédients achetés à l'épicerie chinoise à Grenoble."}</p>
+    <p>{"Les livraisons ne sont pas toujours proposées."}</p>
+    <p>{"La maison correspond à la dernière maison de l'allée en gravier."}</p>
+    <p>{"Les nems sont princialement fait de porc mais la chef peut en faire d'autre si il y eu
+            demande au préalable."}</p>
+    </div>
+    </div>
+    </section>
+            }
+}
+    fn get_html_footer (&self, ctx: &Context<Self>) -> Html{
+        html! {
+        <footer id= "contact" class= "footer">
+    <table class="footer-table">
+      <tbody>
+      <tr class ="tr1">
+        <td class = "icon">
+          <i class="fas fa-phone-alt"></i>
+        </td>
+        <td class="info">
+          <p>{"01.02.03.04.05"}</p>
+        </td>
+      </tr>
+      <tr class ="tr2">
+        <td class = "icon">
+          <i class="fas fa-map-marker-alt"></i>
+        </td>
+        <td class="info">
+          <p>{"18 rue Trump"}</p>
+        </td>
+      </tr>
+      <tr class ="tr3">
+      <td class = "icon">
+        <i class="fas fa-clock"></i>
+      </td>
+      <td class="info">
+        <p> {"Livraison et à Emporter"}</p>
+      </td>
+      </tr>
+      </tbody>
     </table>
-    }
-
-}
-    fn get_html_acceuil(&self, ctx: &Context<Self>) -> Html{
-    html ! {
-    < header >
-    < div class = "nav-img" >
-    <div class = "img-pres" >
-    < / div >
-    <div class = "title" >
-    < h1 >{"Sô Nems"}< / h1 >
-    < hr color = "black" />
-    < h2 >{"spécialité maison"}< / h2 >
-    < / div >
-    < /div >
-    < / header >
-    }
+  </footer>
+        }
     }
 }
-
 
 
 impl Component for App {
@@ -155,9 +283,7 @@ impl Component for App {
     type Properties = ();
 
     fn create(_ctx: &Context<Self>) -> Self {
-        Self { value: 0, videos: Vec::new(), products: Vec::new(), commande:Vec::new()}
-
-
+        Self { value: 0, videos: Vec::new(), products: Vec::new(), commande: Vec::new() }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
@@ -258,13 +384,15 @@ impl Component for App {
     }
 
 
-
-
     fn view(&self, ctx: &Context<Self>) -> Html {
         let videos = self.get_html_member_list(ctx);
         let products = self.get_html_product_list(ctx);
         let commande = self.get_html_cmd_list(ctx);
-        let acceuil = self.get_html_acceuil(ctx);
+        let accueil = self.get_html_accueil(ctx);
+        let navbar = self.get_html_nav(ctx);
+        let concept = self.get_html_concept(ctx);
+        let faq = self.get_html_faq(ctx);
+        let footer = self.get_html_footer(ctx);
 
         html! {
             <>
@@ -273,142 +401,26 @@ impl Component for App {
 
 
 
-<header>
-  <nav class="navbar-part">
-    <div class="container">
-      <div class="navbar-content">
-        <a href="#">
-          <img src="./img/nems-logo.jpg" alt=""/>
-        </a>
-        <h1> {"Sô Nems.fr"}</h1>
-
-
-        <div class="navbar-links">
-          <ul class="navbar-link">
-            <a href="index.html">
-              <li class="navbar-item"> {"Acceuil"}</li>
-            </a>
-            <a href="La-carte.html">
-              <li class="navbar-item">{"La Carte"}</li>
-            </a>
-            <a href="Annonce">
-              <li class="navbar-item">{"Annonce"}</li>
-            </a>
-            <a href="#contact">
-              <li class="navbar-item">{"Contact"}</li>
-            </a>
-            <a href="mon-compte.html">
-              <li class="fifth-link">{"Mon Compte"}</li>
-            </a>
-            <a href = "#FAQbis"> {"FAQ"} </a>
-          /*  <a href="test-yew.html">
-              <li class="fifth-link">{"test yew"}</li>
-            </a>
-            <a href="content_compte.html">
-              <li class="fifth-link">{"contenu d'un compte"}</li>
-            </a>*/
-            <a href = "#" onclick={ctx.link().callback(|_| Msg::GetMembers)}> {"liste des membres"}</a>
-             <a href = "#" onclick={ctx.link().callback(|_| Msg::GetCommande)}> {"Commande"}</a>
-             <a href = "#" onclick={ctx.link().callback(|_| Msg::GetProducts)}> {"liste produit"}</a>
-
-
-
-          </ul>
-
-        </div>
-      </div>
-    </div>
-  </nav>
-
-</header>
-{acceuil}
+{navbar}
+{accueil}
 
 
 <main>
   <section id="Acceuil">
-    <div class="Colonne">
-      <div class="Concept">
-        <div class="desc-conc">
-          <h2>{"Sô Nems ?"}</h2>
-          <p>{"**************************************** "}</p>
-          <p>{"******************************************."}</p>
-          <p>{"***************************************************"}</p>
-        </div>
-
-
-      </div>
-      <div class="Livraison">
-        <div class="desc-livr">
-          <h2> {"Livraison"} </h2>
-          <p>{"Perimètre de livraison : Crolles"}</p>
-          <p>{"Numéro livreur : 01.02.03.04.05"}</p>
-        </div>
-      <div class="img-livr"><img src="./img/dark-scoot.png"/></div>
-
-
-      </div>
-    </div>
-    <section id = "FAQbis">
-      <div class="FAQ">
-        <div class="FAQ-content">
-          <h2> {"F.A.Q"}</h2>
-          <p>{"Les ingrédients achetés à l'épicerie chinoise à Grenoble."}</p>
-          <p>{"Les livraisons ne sont pas toujours proposées."}</p>
-          <p>{"La maison correspond à la dernière maison de l'allée en gravier."}</p>
-          <p>{"Les nems sont princialement fait de porc mais la chef peut en faire d'autre si il y eu
-            demande au préalable."}</p>
-        </div>
-      </div>
-           {videos}
+            {concept}
+            {faq}
+            {videos}
             {products}
             {commande}
-            /*<div class="video">
-             <h3>{"Affichage member"}</h3>
-
-            {videos}
-
-             <h3>{"Fin affichage member"}</h3>
-         </div>*/
-
+            {footer}
     </section>
-  </section>
-  <footer id= "contact" class= "footer">
-    <table class="footer-table">
-      <tbody>
-      <tr class ="tr1">
-        <td class = "icon">
-          <i class="fas fa-phone-alt"></i>
-        </td>
-        <td class="info">
-          <p>{"01.02.03.04.05"}</p>
-        </td>
-      </tr>
-      <tr class ="tr2">
-        <td class = "icon">
-          <i class="fas fa-map-marker-alt"></i>
-        </td>
-        <td class="info">
-          <p>{"18 rue Trump"}</p>
-        </td>
-      </tr>
-      <tr class ="tr3">
-      <td class = "icon">
-        <i class="fas fa-clock"></i>
-      </td>
-      <td class="info">
-        <p> {"Livraison et à Emporter"}</p>
-      </td>
-      </tr>
-      </tbody>
-    </table>
-  </footer>
+
 </main>
 </>
 
     }
     }
 }
-
 
 
 fn main() {
